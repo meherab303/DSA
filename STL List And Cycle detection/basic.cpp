903574fc1e33581e15976e73aa33642e63ecfb37
meherab nahin <nahinrahman87@gmail.com>
2025-07-27 10:37:55 +0600

    stl list basic

---
+++ STL List And Cycle detection/basic.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  
+  list<int>l={1,2,3,4,5,6};
+
+//   for(auto it=l.begin();it!=l.end();it++){
+//     cout<<*it<<endl;
+//   } 
+
+    for(int val:l){
+        cout<<val<<endl;
+    }
+  return 0;
+}
