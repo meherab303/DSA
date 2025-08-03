188410ce37f659f201b0671531cac6efe0bf7f49
meherab nahin <nahinrahman87@gmail.com>
2025-08-03 21:33:33 +0600

    stack implementation using vector

---
+++ Stack/basic.cpp
@@ -0,0 +1,3 @@
+// stack is a data structer that uses LIFO(last in first out) technique that means inserting and deleting can be applied only at top.
+//stack is a abstract data structure.higher level data structure.
+// eita alada kono data structure na .borong lower level data structer use kore eita create kora hoi like array ,linked list etc.
