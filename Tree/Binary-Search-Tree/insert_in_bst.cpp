53042be4d1306b19192247e54f53eefcf4e0cb80
meherab nahin <nahinrahman87@gmail.com>
2025-08-19 13:49:12 +0600

    insertion in bst

---
+++ Tree/Binary-Search-Tree/insert_in_bst.cpp
@@ -0,0 +1,102 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+
+    public:
+    int val;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        this->val=val;
+        this->left=NULL;
+        this->right=NULL;
+    }
+
+
+};
+Node* input_tree(){
+    int val;cin>>val;
+    Node* root=NULL;
+    if(val!=-1)root=new Node(val);
+    queue<Node*>q;
+
+    if(root)
+        q.push(root);
+    while(!q.empty()){
+        Node* parent=q.front();
+        q.pop();
+
+        int l,r;cin>>l>>r;
+        Node* myLeft=NULL;
+        Node* myRight=NULL;
+        if(l!=-1) myLeft=new Node(l);
+        if(r!=-1)myRight=new Node(r);
+
+        parent->left=myLeft;
+        parent->right=myRight;
+
+        if(parent->left)
+            q.push(myLeft);
+        if(parent->right)
+            q.push(myRight);    
+    }    
+    return root;
+
+
+}
+
+void lever_order(Node* root){
+    if(root==NULL){
+        cout<<"No Tree"<<endl;
+         return;
+    }
+       
+    queue<Node*>q;
+    if(root)
+        q.push(root);
+    while(!q.empty()){
+        Node* parent=q.front();
+        q.pop();
+
+        cout<<parent->val<<" ";
+
+        if(parent->left)
+            q.push(parent->left);
+        if(parent->right)
+            q.push(parent->right);    
+    }    
+
+}
+
+void insert_in_bst(Node* &root,int x){
+    if(root==NULL){
+        root=new Node(x);
+        return;
+    }
+    if(root->val<x){
+        if(root->right==NULL){
+            root->right=new Node(x);
+        }else{
+            insert_in_bst(root->right,x);
+        }
+    }else{
+        if(root->left==NULL){
+            root->left=new Node(x);
+        }else{
+            insert_in_bst(root->left,x);
+        }
+    }
+}
+int main()
+{
+  Node* root=input_tree();
+
+
+  int value;cin>>value; 
+  insert_in_bst(root,value);
+  lever_order(root); 
+  
+   
+  return 0;
+}
